dbc26f499cfbea89a975ddb60cf78384aedfbbdf
Nguyen Truong Giang <giang.nguyen@crystal-techs.com>
2020-10-16 11:45:09 +0700

    commit stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import './App.css';
-import AddTodo from './features/todo/AddTodo'
-import TodoList from './features/todo/TodoList'
-import VisibilityFilters from './features/todo/VisibilityFilters'
-import { Counter } from './features/counter/Counter'
 
-
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-      <AddTodo />
-      <VisibilityFilters />
-      <TodoList />
-    </div >
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App" >
+        Hello
+      </div>
+    );
+  }
 }
 
 export default App;
