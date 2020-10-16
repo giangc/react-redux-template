f9afd6793b872cb8f854ed3223ce5e20aaa3fb0c
Nguyen Truong Giang <giang.nguyen@crystal-techs.com>
2020-10-16 11:19:46 +0700

    init blank project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  const { getByText } = render(<App appName="My App" />);
+  expect(getByText(/My App/i)).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css';
-import AddTodo from './features/todo/AddTodo'
-import TodoList from './features/todo/TodoList'
-import VisibilityFilters from './features/todo/VisibilityFilters'
-import { Counter } from './features/counter/Counter'
 
-
-function App() {
+function App(props: any) {
   return (
     <div className="App">
-      <Counter />
-      <AddTodo />
-      <VisibilityFilters />
-      <TodoList />
+      Application {props.appName}
     </div >
   );
 }
