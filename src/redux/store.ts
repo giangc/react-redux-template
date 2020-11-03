3e833fc7f295694ebaf019dd5fd978904699b951
Nguyen Truong Giang <giang.nguyen@crystal-techs.com>
2020-11-03 15:03:44 +0700

    config store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counter/counterSlice';
+import pocketReducer from './financepocket/index'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    financialPocket: pocketReducer
   },
 });
 
